0d8cabaab47b1376f0e1ecd16b5e9759e010c7d0
Charles <enahoroomonkhobhioriacharles@gmail.com>
2022-08-05 13:50:45 +0100

    miles to kilometer converter

---
+++ milesToKilo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# This program accepts input as mile from users and converts to kilometer
+mile = input("Enter the value in mile: ")
+mile = int(mile)
+kilometer = mile * 1.60934
+print(f"{mile} miles equals {kilometer} kilometers")
+
