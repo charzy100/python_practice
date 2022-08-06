0500b97f43dccf570029fd0ff6bf9f31046e02ce
Charles <enahoroomonkhobhioriacharles@gmail.com>
2022-08-06 01:44:05 +0100

    gradesByAge.py

--- gradesByAge.py
+++ gradesByAge.py
@@ -1,6 +1,8 @@
 # Write a program that accepts input from user as age and places them in a school grade based on their input.Your codes should be 10 lines or less.
 age = eval(input("Enter your age: "))
-if age == 5:
+if age < 5:
+    print("Go home till you are 5")
+elif age == 5:
     print("You should be in Kindergaten")
 elif (age >= 6) and (age <= 17):
     print("You should be in grade school from 1 through 12")
